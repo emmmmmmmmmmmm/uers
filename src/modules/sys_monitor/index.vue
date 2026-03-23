a0e4d721a0022c7a8063544dfa63bc4acd024480
emmmmmm <826112587@qq.com>
2026-03-23 15:21:12 +0800

    初始化

--- src/modules/sys_monitor/index.vue
+++ src/modules/sys_monitor/index.vue
@@ -3,7 +3,7 @@
     <div class="search-wrapper">
       <un-form inline>
         <un-form-item label="开始日期">
-          <un-data-picker
+          <un-date-picker
             v-model="opStartDate"
             value-format="yyyy-MM-dd"
             format="yyyy-MM-dd"
@@ -13,13 +13,13 @@
           />
         </un-form-item>
         <un-form-item label="结束日期">
-          <un-data-picker
+          <un-date-picker
             v-model="opEndDate"
             value-format="yyyy-MM-dd"
             format="yyyy-MM-dd"
             type="date"
             placeholder="请选择结束日期"
-            :picker-options="startPickerOptions"
+            :picker-options="endPickerOptions"
           />
         </un-form-item>
         <un-form-item>
@@ -29,32 +29,33 @@
       </un-form>
     </div>
     <un-table
-        :data="tableData"
-        style="width: 100%"
-        stripe
-        :default-sort="{prop: 'op_time', order: 'descending'}"
-        :row-style="{'height':'40px'}"
-        :header-row-style="{'height':'40px'}"
-        :class="['limitmaxheight',{isBorder: tableData.length === 0}]"
+      :data="tableData"
+      style="width: 100%"
+      stripe
+      :default-sort="{ prop: 'op_time', order: 'descending' }"
+      :row-style="{ height: '40px' }"
+      :header-row-style="{ height: '40px' }"
+      :class="['limitmaxheight', { isBorder: tableData.length === 0 }]"
+    >
+      <un-table-column prop="op_time" label="日期" sortable width="180" align="center"></un-table-column>
+      <un-table-column prop="op_user" label="操作人ID" sortable width="180" align="center"></un-table-column>
+      <un-table-column prop="name" label="操作人姓名" sortable width="180" align="center"></un-table-column>
+      <un-table-column prop="op_detail" label="日志内容" align="center"></un-table-column>
+    </un-table>
+    <div class="pageDiv">
+      <div class="totalNum">总共{{ totalNum }}个项目</div>
+      <un-pagination
+        background
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="curPage"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="pageSize"
+        layout="prev, pager, next, sizes, jumper"
+        :total="totalNum"
       >
-        <un-table-column prop="op_time" label="日期"2 sortable width="180" align="center"></un-table-column>
-        <un-table-column prop="op_user" label="操作人ID" sortable width="180" align="center"></un-table-column>
-        <un-table-column prop="name" label="操作人姓名" sortable width="180" align="center"></un-table-column>
-        <un-table-column prop="op_detail" label="日志内容" align="center"></un-table-column>
-      </un-table>
-      <div class="pageDiv">
-        <div class="totalNum">总共{{totalNum}}个项目</div>
-        <un-pagination
-          background
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="curPage"
-          :page-sizes="[10, 20, 30, 40]"
-          :page-size="pageSize"
-          layout="prev, pager, next, sizes, jumper"
-          :total="totalNum">
-        </un-pagination>
-      </div>
+      </un-pagination>
+    </div>
   </div>
 </template>
 
@@ -66,8 +67,8 @@ export default un.component(
   {
     data () {
       return {
-        opStartDate:'',
-        opEndDate:''
+        opStartDate: '',
+        opEndDate: ''
       }
     },
     computed: {
@@ -79,69 +80,68 @@ export default un.component(
           this.setCurrentPage(newVal)
         }
       },
-      startPickerOptions(){
-        return{
-          disableDate:(item)=>{
-            const currentDate = new Date(this.today + ' 00:00:00').getTime
-            const minDate = new Date(this.minStartDate + ' 00:00:00').getTime
-            const endDate = this.opEndDate ? new Date(this.opEndDate + ' 00:00:00').getTime : currentDate
+      startPickerOptions () {
+        return {
+          disabledDate: (time) => {
+            const currentDate = new Date(this.today + ' 00:00:00').getTime()
+            const minDate = new Date(this.minStartDate + ' 00:00:00').getTime()
+            const endDate = this.opEndDate ? new Date(this.opEndDate + ' 00:00:00').getTime() : currentDate
             return time.getTime() < minDate || time.getTime() > endDate
           }
-        }  
+        }
       },
-      endPickerOptions(){
-        return{
-            disableDate:(item)=>{
-            const currentDate = new Date(this.today + ' 00:00:00').getTime
-            const startDate = this.opStartDate ? new Date(this.opStartDate + ' 00:00:00').getTime() : new Date(this.minDate + ' 00:00:00').getTime()
+      endPickerOptions () {
+        return {
+          disabledDate: (time) => {
+            const currentDate = new Date(this.today + ' 00:00:00').getTime()
+            const startDate = this.opStartDate ? new Date(this.opStartDate + ' 00:00:00').getTime() : new Date(this.minStartDate + ' 00:00:00').getTime()
             return time.getTime() < startDate || time.getTime() > currentDate
-             }         
           }
+        }
       },
-      today(){
+      today () {
         return this.formatDate(new Date())
-     },
-     minStartDate(){
-      return this.formatDate(this.getOffsetMontnDate(new Date(), -6))
-     }
+      },
+      minStartDate () {
+        return this.formatDate(this.getOffsetMonthDate(new Date(), -6))
+      }
     },
     created () {
-      this.initDefaulDates()
-      this.handleSearch
-      //this.setCurTableData({start:0, end:this.totalNum});
+      this.initDefaultDates()
+      this.handleSearch()
     },
     methods: {
       formatter (row, column) {
         return row.address
       },
-      formatDate(date){
+      formatDate (date) {
         const year = date.getFullYear()
-        const month = `${date.getMonth() + 1}`.padStart(2,'0')
-        const day = `${date.getDate()}`.padStart(2,0)
-        return `${year} - ${month} - ${day}`
+        const month = `${date.getMonth() + 1}`.padStart(2, '0')
+        const day = `${date.getDate()}`.padStart(2, '0')
+        return `${year}-${month}-${day}`
       },
-      getOffsetMontnDate(baseDate,offsetMonth){
+      getOffsetMonthDate (baseDate, offsetMonth) {
         const date = new Date(baseDate)
         const day = date.getDate()
         date.setDate(1)
         date.setMonth(date.getMonth() + offsetMonth)
-        const lastDay = new Date(date.getFullYear,date.getMonth + 1,0).getDate()
-        date.setDate(Math.min(day,lastDay))
+        const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+        date.setDate(Math.min(day, lastDay))
         return date
       },
-      initDefaulDates(){
+      initDefaultDates () {
         this.opEndDate = this.today
-        this.opStartDate = this.formatDate(this.getOffsetMontnDate(new Date(),-5))
+        this.opStartDate = this.formatDate(this.getOffsetMonthDate(new Date(), -5))
       },
-      handleSearch(){
+      handleSearch () {
         this.setCurrentPage(1)
         this.getInfo({
           opStartDate: this.opStartDate,
           opEndDate: this.opEndDate
         })
       },
-      resetSearch(){
-        this.initDefaulDates()
+      resetSearch () {
+        this.initDefaultDates()
         this.handleSearch()
       },
       handleSizeChange (size) {
@@ -194,21 +194,21 @@ export default un.component(
 
 <style lang="scss">
 @media only screen and(min-height:801px) {
-  .limitmaxheight.un-table td, .limitmaxheight.un-table th{
+  .limitmaxheight.un-table td, .limitmaxheight.un-table th {
     padding: 0px;
   }
 }
 .sys-monitor-wrap {
-  .search-wrapper{
+  .search-wrapper {
     margin-bottom: 16px;
 
-    .un-form{
+    .un-form {
       display: flex;
       flex-wrap: wrap;
       align-items: center;
     }
 
-    .un-form-item{
+    .un-form-item {
       margin-bottom: 12px;
       margin-right: 16px;
     }
@@ -226,7 +226,7 @@ export default un.component(
     }
     .un-table {
       &.limitmaxheight {
-        td,th{
+        td, th {
           padding: 0;
         }
       }
@@ -270,27 +270,27 @@ export default un.component(
     }
   }
 }
-.pageDiv{
+.pageDiv {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  .totalNum{
+  .totalNum {
     font-size: 14px;
     color: #999999;
   }
   .un-pagination.is-background {
     margin-top: 16px;
-    .btn-next, .btn-prev, .un-pager li{
+    .btn-next, .btn-prev, .un-pager li {
       min-width: 28px;
       height: 28px;
       line-height: 28px;
     }
     .un-select {
       .un-input {
-          .un-input__inner{
+        .un-input__inner {
           line-height: 10px;
-          }
         }
+      }
     }
   }
 }
